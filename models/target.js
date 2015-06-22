727581e57c1a831bf4a4486d15226da54010e9f1
Mart <martvanblokland@hotmail.com>
2015-06-22 23:28:49 +0200

    added basic lemniscate in target

--- models/target.js
+++ models/target.js
@@ -11,13 +11,20 @@ module.exports = {
 
 	var time = new Date().getTime();
 
-	// basic 
+	//////////// basic up and down ///////////////////////
 	var amp = 1000; // amplitude in mm
 	var freq = 5 * 1000; //frequency in ms 
 	var bottom = 1000; //bottom in mm
 	var sin = amp * Math.sin((2*Math.PI * time) / freq) + (amp + bottom); 
+    return { x: (-500 + 500 * id), y: -1500, z: sin, yaw: 0 };
 
-	return { x: (-500 + 500 * id), y: -1500, z: sin, yaw: 0 };
-    // return { x: 250, y:-1500, z:1800, yaw: 0 };
+    ///////////// basic lemniscate ///////////////////////
+    // var xVar = 2000; //variation in the x-direction
+    // var yVar = 1000; //variation in the y-direction
+    // var freq = 5 * 1000; //frequency in ms 
+    // var denominator = (Math.pow(Math.sin((2*Math.PI * time) / freq), 2) + 1) ; 
+    // var xTarget = (xVar * Math.cos((2*Math.PI * time) / freq)) / denominator ; 
+    // var yTarget = (yVar * Math.cos((2*Math.PI * time) / freq) * Math.sin((2*Math.PI * time) / freq)) / denominator); 
+    // return {x : xTarget, y : yTarget, z = 1500, yaw : 0};
   },
 };
