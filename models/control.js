0da6d5e51357244ee253aac63f0956767436000e
Martin van Harmelen <Martin@vanharmelen.com>
2015-06-23 11:23:30 +0200

    Wrote control function

--- models/control.js
+++ models/control.js
@@ -10,6 +10,20 @@ var VelocityToPower = function(reqV){
  return c[0] + c[1] * reqV; // + c[2] * reqV^2;
 }
 
+var GetControlVelocity = function(reqV, currentV){
+    return reqV - currentV;
+}
+
+var GetRequiredVelocity = function(distance) {
+    var THRESHOLD_DISTANCE = 1000; // in mm
+    var MAX_SPEED = 0.6;
+
+    if (distance > THRESHOLD_DISTANCE)
+        return MAX_SPEED;
+    else
+        return MAX_SPEED * Math.abs(distance) * distance / Math.pow(THRESHOLD_DISTANCE, 2);
+}
+
 module.exports = function(target, mocap){
     // console.log(target.x);
     // console.log(target.y);
@@ -17,7 +31,17 @@ module.exports = function(target, mocap){
     // console.log(mocap.p.x);
     // console.log(mocap.p.y);
     // console.log(mocap.p.z);
-
-
-    return { vx: 0, vy: 0, vz: 0, vYaw: 0 };
+    // console.log(mocap.v.vx);
+    // console.log(mocap.v.vy);
+    // console.log(mocap.v.vz);
+    var axes = ["x", "y", "z"];
+    var result = {vYaw: 0};
+    for (var i = 0; i < axes.length; i++) {
+        var axis = axes[i];
+        distance = target[axis] - mocap.p[axis];
+        var reqV = GetRequiredVelocity(distance);
+        result["v" + axis] = GetControlVelocity(reqV, mocap.v["v" + axis]);
+    };
+    // console.log(result);
+    return result;
 }
